a26ce7cef064f9f2584b863755ecfdf5efd17204
Nikita Sviridenko <nikita.sviridenko@voypost.com>
2024-12-06 13:08:08 +0100

    Fix

--- browser_use/browser/context.py
+++ browser_use/browser/context.py
@@ -534,6 +534,14 @@ class BrowserContext:
 		"""
 		page = await self.get_current_page()
 
+		print('Internal zoom out')
+		# Make sure we can zoom out the page
+		# Sample: https://ezamowienia.gov.pl/mp-client/search/list/ocds-148610-71f17e58-2c45-41e1-890d-8b58d41b3059
+		await page.evaluate("""
+				document.documentElement.style.height = 'auto';
+				document.body.style.height = 'auto';
+		"""),
+		print('Taking screenshot')
 		screenshot = await page.screenshot(
 			full_page=full_page,
 			animations='disabled',
